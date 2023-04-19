753c1ec0ecd3f3cea95c8ad5dc53e176ebf012cf
Aubrey Moya-Mendez Trotter <Aubrey@AdminisatorsMBP.lan>
2023-04-18 21:30:54 -0400

    Completed 9ReturnBoolean challenge

--- 8RemoveSpaces/RemoveSpaces.js
+++ 8RemoveSpaces/RemoveSpaces.js
@@ -1,7 +1,7 @@
 // 8. Remove the spaces found in a string
 // Yet another way to clean up data is to remove any errors or unnecessary spaces. This function will take in a string and then return it with all spaces removed. Think about if you were tasked with cleaning up customer data at your job. You could scale this function to clean up specific fields of data, such as zip codes.
 
-//Define my string
+// Define my string
 const myStrng = 'Hello, welcome to my RemoveSpaces challenge!';
 
 //Define removeSpaces and use replaceAll to take out spaces (To remove every instance of the letter a, you could use .replaceAll('a', '') or to replace all instances of the letter a with the letter b, you could use replaceAll('a', 'b'))
@@ -9,6 +9,18 @@ const removeSpaces = myStrng.replaceAll(' ', '');
 
 console.log(removeSpaces);
 
+
 // Or this can be done without creating the removeSpaces variable and implementing the methon inside the console.log
 
 console.log(myStrng.replaceAll(' ', ''));
+
+//Or this can be done with a function
+
+function rmvSpaces(funStrng) {
+    return funStrng.replaceAll(' ', '');
+  }
+
+const funStrng = 'Hello, welcome to my RemoveSpaces challenge using a function!'
+
+
+console.log(rmvSpaces(funStrng));
--- 9ReturnBoolean/ReturnBoolean.js
+++ 9ReturnBoolean/ReturnBoolean.js
@@ -1,15 +1,24 @@
 //9. Return a Boolean if a number is divisible by 10
 //Here, you’ll create a function that’ll give you a “true” or “false” Boolean as its output. The inputted number should only return a “true” if it’s divisible by 10. Otherwise, your program should return a “false” answer.
 
-// let num = 101;
-// let isDivisible10 = (num % 10 == 0);
+let num = 101;
+let isDivisible10 = (num % 10 == 0);
 
-// console.log(isDivisible10);
+console.log(isDivisible10);
 
-//Using a function***STILL WORKING****
+//Using a function
 
 var num1 = 100;
  
-function divisible(num1) {
-    if (num1 % 10 == 0 ){return console.log(true);
-    } else {return console.log(false);}}
+//create function
+function divisible(number) {
+    //use a conditional to return true or false
+    if (number % 10 == 0) {
+        return true;
+      } else {
+        return false;
+      }
+};
+
+//call function with console.log to display in console
+console.log(divisible(num1));
